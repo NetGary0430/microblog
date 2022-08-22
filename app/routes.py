dc5b574b45f2e32ff02cd4cf273b41bcdc3a8d9e
Gary Netherton <garynetherton@gmail.com>
2022-08-21 17:24:39 -0700

    Added 'render_template function from Flask'

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+from flask import render_template
+
 from app import app
 
 
@@ -8,4 +10,6 @@ from app import app
 @app.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
+
