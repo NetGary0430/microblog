196cb885b02164fb33ad811c1cd22b6e94dc3479
Gary Netherton <garynetherton@gmail.com>
2022-09-04 14:22:11 -0700

    Added login using Flask_WTF and FlaskForm

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,26 @@
 from flask import render_template
-
 from app import app
+from app.forms import LoginForm
 
-
-# the @app.route is a decorator which modifies the function that follows it.
-# common pattern:  use them to register functions as callbacks for certain events
 
 @app.route('/')
 @app.route('/index')
-
 def index():
     user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
